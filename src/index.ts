4ac74ddce5dace76cfac2a6ee74b73218ecf7514
Platos69 <danielfelipe2905@gmail.com>
2024-03-19 19:51:31 -0300

    added new configs of index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { server } from './server/Server';
 
-const port = 3333;
+const port = process.env.PORT;
 
 server.listen(port, () => {
     console.log('App rodando! http://localhost:' + port);
